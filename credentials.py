16baba3bbd9df0e117364a636032ec64a5276db4
Sachin Dolta <imsdolta@gmail.com>
2019-02-17 11:49:42 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,19 +1,7 @@
 keys = dict(
-    consumer_key =          'Res8PgHsea6ZCoFenrT7XBl5E',
-    consumer_secret =      'wtpjNYiNXyJRCx3toDs1qNNRxeW0ImotCZy9kCWrRVDmKQqObc',
-    access_token =          '720146741886984192-AvWXyWThKLfB4OlZ6F9leMIpHobwtWY',
-    access_token_secret =   'oc4F6QaDsbeOdCtZxipw7Fg6SR20V33oiwtT71Kev2yde',
+    consumer_key =          'consumer key',
+    consumer_secret =      'consumer_secret,
+    access_token =          'access_token',
+    access_token_secret =   'access_token_sceret',
 )
 
-keys2 = dict(
-  consumer_key =          'Res8PgHsea6ZCoFenrT7XBl5E',
-    consumer_secret =      'wtpjNYiNXyJRCx3toDs1qNNRxeW0ImotCZy9kCWrRVDmKQqObc',
-    access_token =          '720146741886984192-AvWXyWThKLfB4OlZ6F9leMIpHobwtWY',
-    access_token_secret =   'oc4F6QaDsbeOdCtZxipw7Fg6SR20V33oiwtT71Kev2yde',
-)
-keys3 = dict(
-    consumer_key =          'Res8PgHsea6ZCoFenrT7XBl5E',
-    consumer_secret =      'wtpjNYiNXyJRCx3toDs1qNNRxeW0ImotCZy9kCWrRVDmKQqObc',
-    access_token =          '720146741886984192-AvWXyWThKLfB4OlZ6F9leMIpHobwtWY',
-    access_token_secret =   'oc4F6QaDsbeOdCtZxipw7Fg6SR20V33oiwtT71Kev2yde',
-)
